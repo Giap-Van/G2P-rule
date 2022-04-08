4afe7aeb727e41189a0fa0eac9c53075d2a5a646
Giap-Van <56864229+Giap-Van@users.noreply.github.com>
2022-04-08 15:21:12 +0700

    Improving with new_vowels_file

--- G2P_Function.py
+++ G2P_Function.py
@@ -1,49 +1,23 @@
 import os, json
 from collections import defaultdict
 
+from symbols import _specials
+from symbols import _silent
+
 def load_g2p_consonants():
     p2g = defaultdict(list)
-    with open('G2P_consonants.json','r') as f:
+    with open('/home/vangt/Project/text/G2P_consonants.json','r') as f:
         p2g = json.load(f)
     return p2g
 
 def load_g2p_vowels():
     p2g = defaultdict(list)
-    with open('G2P_vowels.json','r') as f:
+    with open('/home/vangt/Project/text/g2p_vowels.json','r') as f:
         p2g = json.load(f)
     return p2g
 
-def load_g2p_falling_tone():
-    p2g = defaultdict(list)
-    with open('G2P_falling_tone.json','r') as f:
-        p2g = json.load(f)
-    return p2g
 
-def load_g2p_curve_tone():
-    p2g = defaultdict(list)
-    with open('G2P_curve_tone.json','r') as f:
-        p2g = json.load(f)
-    return p2g
-
-def load_g2p_broken_tone():
-    p2g = defaultdict(list)
-    with open('G2P_broken_tone.json','r') as f:
-        p2g = json.load(f)
-    return p2g
-
-def load_g2p_rising_tone():
-    p2g = defaultdict(list)
-    with open('G2P_rising_tone.json','r') as f:
-        p2g = json.load(f)
-    return p2g
-
-def load_g2p_drop_tone():
-    p2g = defaultdict(list)
-    with open('G2P_drop_tone.json','r') as f:
-        p2g = json.load(f)
-    return p2g
-
-def G2P_word(word, consonants, vowels, g2p_consonants, g2p_vowels, fallings, curves, brokens, risings, drops):
+def G2P_word(word, consonants, g2p_consonants, g2p_vowels, vowels):
     # print(consonants)
     # print(vowels)    
     p_word = []
@@ -54,7 +28,7 @@ def G2P_word(word, consonants, vowels, g2p_consonants, g2p_vowels, fallings, cur
             id += len(initial)
             phoneme = g2p_consonants[initial]
             if initial == 'gi':
-                if (id > len(word)) or (id <= len(word) and word[id] == 'ê'):
+                if (id >= len(word)) or (id < len(word) and word[id] == 'ê'):
                     p_word.append('zi')
                 else:
                     p_word.append(phoneme[0])
@@ -113,77 +87,33 @@ def G2P_word(word, consonants, vowels, g2p_consonants, g2p_vowels, fallings, cur
             else:
                 p_word.append(phoneme[0])
             break
-    
-    tone = '1'
-    for falling in fallings:
-        if falling in word:
-            tone = '2'
-            break
-    for curve in curves:
-        if curve in word:
-            tone = '3'
-            break
-    for broken in brokens:
-        if broken in word:
-            tone = '4'
-            break
-    for rising in risings:
-        if rising in word:
-            if word.endswith("p") or word.endswith("t") or word.endswith("ch") or word.endswith("c"):
-                tone = '5b'
-            else:
-                tone = '5a'
-    for drop in drops:
-        if drop in word:
-            if word.endswith("p") or word.endswith("t") or word.endswith("ch") or word.endswith("c"):
-                tone = '6b'
-            else:
-                tone = '6a'
 
-    p_word.append(tone)
-    pword = '-'.join(p_word)
-    return pword
+    
+    return p_word
 
 def G2P_text(text):
     g2p_consonants = load_g2p_consonants()
     g2p_vowels = load_g2p_vowels()
-    falling_tone = load_g2p_falling_tone()
-    curve_tone = load_g2p_curve_tone()
-    broken_tone = load_g2p_broken_tone()
-    rising_tone = load_g2p_rising_tone()
-    drop_tone = load_g2p_drop_tone()
 
     consonants = []
     vowels = []
-    fallings = []
-    curves = []
-    brokens = []
-    risings = []
-    drops = []
 
     for key, value in g2p_consonants.items():
         consonants.append(key)
     for key, value in g2p_vowels.items():
         vowels.append(key)
-    for key, value in falling_tone.items():
-        fallings.append(key)
-    for key, value in curve_tone.items():
-        curves.append(key)
-    for key, value in broken_tone.items():
-        brokens.append(key)
-    for key, value in rising_tone.items():
-        risings.append(key)
-    for key, value in drop_tone.items():
-        drops.append(key)
-
+   
     text = text.split(' ')
     ptext = []
     for word in text:
-        ptext.append(G2P_word(word, consonants, g2p_consonants, g2p_vowels, vowels, fallings, curves, brokens, risings, drops))
-    ptext = ' '.join(ptext)
+        if word in _specials or word in _silent:
+            ptext.append(word)
+        else:
+            ptext.extend(G2P_word(word, consonants, g2p_consonants, g2p_vowels, vowels))
+    # ptext = ' '.join(ptext)
     return ptext
 
-grapheme = "a ủ lá chờ ọe uy ích ấy lóe quá trẹo nhãn nhuyễn soát"
+grapheme = "a ủ lá chờ ọe uy ích ấy lóe quá trẹo nhãn nhuyễn soát người"
 phoneme = G2P_text(grapheme)
 print("Grapheme: ", grapheme)
 print("Phoneme: ", phoneme)
